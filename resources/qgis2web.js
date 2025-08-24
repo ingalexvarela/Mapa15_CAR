3fff30611f285e70a664b90ee987ea540e4c96bf
Alex Varela Quirós <98862101+ingalexvarela@users.noreply.github.com>
2025-08-24 15:29:00 -0600

    Update qgis2web.js

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -1493,7 +1493,7 @@ rosaContainer.style.zIndex = "1000";
 
 
 var rosaImg = document.createElement("img");
-rosaImg.src = "https://github.com/ingalexvarela/Mapa15_CAR/blob/main/images/rosaT.png";
+rosaImg.src = "https://raw.githubusercontent.com/ingalexvarela/Mapa15_CAR/main/images/rosaT.png";
 rosaImg.style.width = "140px";
 rosaImg.style.height = "auto";
 rosaImg.style.opacity = "0.9";
